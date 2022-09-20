2c540e108c791d6260e4533ef2b33cb2e1c8015c
Onnonoka <frederic.dymko@gmail.com>
2022-09-20 14:26:53 +0200

    init

---
+++ StateMachine/src/StateMachineManipulation.java
@@ -0,0 +1,104 @@
+import java.util.Scanner;
+
+import org.eclipse.emf.common.util.TreeIterator;
+import org.eclipse.emf.common.util.URI;
+import org.eclipse.emf.ecore.EObject;
+import org.eclipse.emf.ecore.EPackage;
+import org.eclipse.emf.ecore.resource.Resource;
+import org.eclipse.emf.ecore.resource.impl.ResourceSetImpl;
+import org.eclipse.emf.ecore.xmi.XMLResource;
+import org.eclipse.emf.ecore.xmi.impl.XMIResourceFactoryImpl;
+import org.eclipse.emf.ecore.xmi.impl.XMLMapImpl;
+
+import SimplStateMachine.*;
+
+public class StateMachineManipulation {
+
+	public void sauverModele(String uri, EObject root) {
+		Resource resource = null;
+		try {
+			URI uriUri = URI.createURI(uri);
+			Resource.Factory.Registry.INSTANCE.getExtensionToFactoryMap().put("xmi", new XMIResourceFactoryImpl());
+			resource = (new ResourceSetImpl()).createResource(uriUri);
+			resource.getContents().add(root);
+			resource.save(null);
+		} catch (Exception e) {
+			System.err.println("ERREUR sauvegarde du modèle : "+e);
+			e.printStackTrace();
+		}
+	}
+
+	public Resource chargerModele(String uri, EPackage pack) {
+		Resource resource = null;
+		try {
+			URI uriUri = URI.createURI(uri);
+			Resource.Factory.Registry.INSTANCE.getExtensionToFactoryMap().put("xmi", new XMIResourceFactoryImpl());
+			resource = (new ResourceSetImpl()).createResource(uriUri);
+			XMLResource.XMLMap xmlMap = new XMLMapImpl();
+			xmlMap.setNoNamespacePackage(pack);
+			java.util.Map options = new java.util.HashMap();
+			options.put(XMLResource.OPTION_XML_MAP, xmlMap);
+			resource.load(options);
+		}
+		catch(Exception e) {
+			System.err.println("ERREUR chargement du modèle : "+e);
+			e.printStackTrace();
+		}
+		return resource;
+	}
+
+	public StateMachine getModelBase(String modelFile) {
+		Resource resource = chargerModele(modelFile, SimplStateMachinePackage.eINSTANCE);
+		if (resource == null) {
+			System.err.println(" Erreur de chargement du modèle");
+			return null;
+		}
+
+		TreeIterator<EObject> it = resource.getAllContents();
+
+		StateMachine sm = null;
+		while(it.hasNext()) {
+			EObject obj = (EObject) it.next();
+			if (obj instanceof StateMachine) {
+				sm = (StateMachine) obj;
+				break;
+			}
+		}
+		return sm;
+	}
+
+
+	public void initStateMachine(StateMachine sm) {
+		// TODO
+	}
+	
+	public void processEvent(StateMachine sm, String event) {
+		// TODO
+	}
+	
+	public void executeStateMachine(StateMachine sm) {
+		
+		this.initStateMachine(sm);
+		
+		Scanner scan = new Scanner(System.in);
+        String event = "";
+       
+        while (!event.equals("end")) {
+        	System.out.print("Entrez le nom d'un événement (\"end\" pour terminer) : ");
+            event = scan.nextLine();
+            if (!event.equals("end")) processEvent(sm,event);
+        }
+		
+	}
+	
+	public static void main(String argv[]) {
+
+		StateMachineManipulation util = new StateMachineManipulation();
+
+		System.out.println(" Chargement du modèle");
+		StateMachine sm = util.getModelBase("models/Feu.xmi");
+		System.out.println(" Modèle chargé");
+
+		util.executeStateMachine(sm);		
+	}
+}
